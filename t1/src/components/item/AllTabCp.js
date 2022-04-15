7823d505027779c0729219f73d5ce176e8d0a618
dlstn4509 <dlstn7609@hanmail.net>
2022-04-15 10:05:10 +0900

    [Refactor] hover 수정

--- t1/src/components/item/AllTabCp.js
+++ t1/src/components/item/AllTabCp.js
@@ -12,6 +12,7 @@ const AllTabWrap = styled.div`
   padding: 10px 0;
   text-align: center;
   background-color: #f0f3f7;
+  cursor: pointer;
   img {
     vertical-align: middle;
     margin-left: 6px;
@@ -50,54 +51,47 @@ const TtBox = styled.div`
 `;
 
 const AllTabCp = () => {
-  const [tab01, setTab01] = useState(true);
-  const [tab02, setTab02] = useState(false);
-  const [tab03, setTab03] = useState(false);
-  const [tab04, setTab04] = useState(false);
+  const [choiceTab, setChoiceTab] = useState('tab01');
   const [isTtBox, setIsTtbox] = useState(true);
-  const showTtbox = useCallback(() => {
+  const onMouseEvent = useCallback(() => {
     setIsTtbox(!isTtBox);
-  }, []);
-  const hideTtbox = useCallback(() => {
-    setIsTtbox(!isTtBox);
-  }, []);
+  }, [isTtBox]);
   useEffect(() => {
-    showTtbox();
-    hideTtbox();
+    onMouseEvent();
   }, []);
   return (
     <AllTabCpWrapper>
       <AllTabWrap
-        className={tab01 ? 'active' : ''}
+        className={choiceTab === 'tab01' ? 'active' : ''}
         onClick={() => {
-          setTab01(!tab01);
+          setChoiceTab('tab01');
         }}
       >
         <Title>전체</Title>
         <Count>15593</Count>
       </AllTabWrap>
       <AllTabWrap
-        className={tab02 ? 'active' : ''}
+        className={choiceTab === 'tab02' ? 'active' : ''}
         onClick={() => {
-          setTab02(!tab02);
+          setChoiceTab('tab02');
         }}
       >
         <Title>공실매물관</Title>
         <Count>15593</Count>
       </AllTabWrap>
       <AllTabWrap
-        className={tab03 ? 'active' : ''}
+        className={choiceTab === 'tab03' ? 'active' : ''}
         onClick={() => {
-          setTab03(!tab03);
+          setChoiceTab('tab03');
         }}
       >
         <Title>공동중개관</Title>
         <Count>15593</Count>
       </AllTabWrap>
       <AllTabWrap
-        className={tab04 ? 'active' : ''}
+        className={choiceTab === 'tab04' ? 'active' : ''}
         onClick={() => {
-          setTab04(!tab04);
+          setChoiceTab('tab04');
         }}
       >
         <Title>
@@ -105,8 +99,8 @@ const AllTabCp = () => {
           <img
             src={process.env.REACT_APP_URL + 'img/tt_box_icon.png'}
             alt=""
-            onMouseEnter={showTtbox}
-            onMouseLeave={hideTtbox}
+            onMouseEnter={onMouseEvent}
+            onMouseLeave={onMouseEvent}
           />
           {isTtBox && (
             <TtBox>
